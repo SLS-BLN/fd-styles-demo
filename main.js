9da65cce9834fc52a3c31c6c095074fb46f62fc1
Stefan Schlicker <stefanschlicker@posteo.de>
2023-09-12 11:36:45 +0200

    initial setup

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+import "fundamental-styles/dist/button.css";
+import "fundamental-styles/dist/page.css";
+import "fundamental-styles/dist/bar.css";
+import "fundamental-styles/dist/icon.css";
+
+import "fundamental-styles/dist/theming/sap_horizon.css";
+import "@sap-theming/theming-base-content/content/Base/baseLib/sap_horizon/css_variables.css";
